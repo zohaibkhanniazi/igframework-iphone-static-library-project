dca9cbab4fd978218c19759d6dc17934808c4d0b
ondrej.rafaj <ondrej.rafaj@caa8f27d-b2c5-823e-35f6-a1458b116cb6>
2010-03-25 17:58:53 +0000

    adding library output

---
+++ trunk/build/Debug-iphoneos/usr/local/include/IGAlertTextInput.h
@@ -0,0 +1,16 @@
+//
+//  IGAlertTextInput.h
+//  IGFrameworkProject
+//
+//  Created by Ondrej Rafaj on 24.3.10.
+//  Copyright 2010 Home. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface IGAlertTextInput : NSObject {
+
+}
+
+@end
